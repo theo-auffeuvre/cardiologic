5dab850f648b8c7ef00dac0e81d1052ff8b218cf
theo auffeuvre <theo.auffeuvre@gmail.com>
2023-03-07 11:23:12 +0100

    upload mat

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class ConsultationsController < ApplicationController
 
   def new
@@ -8,6 +10,12 @@ class ConsultationsController < ApplicationController
   def create
     @consultation = Consultation.new(consultation_params)
     @consultation.general_practitioner_user = current_user
+    @mat_file = params[:consultation][:file].tempfile unless params[:consultation][:file].nil?
+    @data_ecg = upload_convert(@mat_file)
+    @data_ecg.shift
+    @ecg = Ecg.new(data: @data_ecg.to_json)
+    @ecg.patient = @consultation.patient
+    @ecg.save!
     @consultation.save!
   end
 
@@ -16,4 +24,14 @@ class ConsultationsController < ApplicationController
   def consultation_params
     params.require(:consultation).permit(patient_attributes: [:first_name, :last_name, :birth_date, :height, :weight])
   end
+
+  def upload_convert(mat_file)
+    system("python3 app/python/converter.py")
+    array = []
+    CSV.foreach('test.csv') do |line|
+      array << [line[0], line[1]]
+    end
+    return array
+  end
+ 
 end
