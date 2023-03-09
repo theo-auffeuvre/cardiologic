9cf87fc056932a7a3ec05795f78413526939b687
Agnes Ramiandrisoa <agnesrami@yahoo.fr>
2023-03-09 21:32:16 +0100

    click drop zone

--- app/javascript/controllers/drag_drop_controller.js
+++ app/javascript/controllers/drag_drop_controller.js
@@ -8,20 +8,37 @@ export default class extends Controller {
       console.log("controller drag-drop connecté !");
     }
 
+    /**
+     * On drop zone click, we trigger <input type="file"/> click
+     * to open native file browser on client.
+     */
+    onDropZoneClick(event) {
+      this.inputTarget.click();
+    }
+
+    /**
+     * When file is selected by user in native file browser, input element
+     * will emit 'change' event. We listen to it to update dropZone innerText
+     */
+    onInputChange(event) {
+      console.log('On Input change triggered', this.inputTarget.files[0])
+      this.zoneTarget.innerText = this.inputTarget.files[0]?.name;
+    }
+
     drop(event) {
         event.preventDefault();
         this.inputTarget.files = event.dataTransfer.files;
         this.zoneTarget.innerText = event.dataTransfer.files[0].name;
-      }
+    }
 
-      dragover(event) {
-        this.zoneTarget.classList.add('hover');
-        event.preventDefault();
+    dragover(event) {
+      this.zoneTarget.classList.add('hover');
+      event.preventDefault();
 
-      }
+    }
 
-      dragleave(event) {
-        this.zoneTarget.classList.remove('hover');
-        event.preventDefault();
-      }
+    dragleave(event) {
+      this.zoneTarget.classList.remove('hover');
+      event.preventDefault();
+    }
   }
