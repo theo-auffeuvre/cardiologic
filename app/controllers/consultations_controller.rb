b5d88b71fbd7edaebbd98b1a695713ca8ad504dc
Florent <florent.vincerot@me.com>
2023-03-13 11:25:48 +0100

    capsules alertes

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -30,7 +30,7 @@ class ConsultationsController < ApplicationController
     @data = JSON.parse(@consultation.patient.ecgs.last.data)
     @message = Message.new
     @peeks = peeks_extractor(@data)
-    
+
     # @peeks_QS = peeks_inverse_extractor(@data)
     @intervals_in_ms = get_intervals(@peeks)
 
@@ -56,8 +56,8 @@ class ConsultationsController < ApplicationController
       format.html
       format.text { render partial: "cardiologists", locals: {cardiologists: @cardiologists}, formats: [:html] }
     end
-    
-    # raise 
+
+    # raise
     # redirect_to consultation_path(@consultation)
   end
 
@@ -102,7 +102,7 @@ class ConsultationsController < ApplicationController
     @maxs.each do |peek|
       @peeks << peek.max_by{|k| k[1] }
     end
-    
+
     return @peeks
   end
 
